e2773a269cd8d14dd0fe454516760088d6170ac6
HeyGrabThis <qkqajrrhtj@naver.com>
2024-06-28 22:46:25 +0900

    필드 구성 1

--- client/src/views/field.js
+++ client/src/views/field.js
@@ -1,16 +1,33 @@
 const Field = () => {
   return (
-    <div className='field'>
-      <div className='center-line'></div>
-      <div className='center-circle'></div>
-      <div className='penalty-area top'></div>
-      <div className='penalty-area bottom'></div>
-      <div className='goal-area top'></div>
-      <div className='goal-area bottom'></div>
-      <div className='goal top'></div>
-      <div className='goal bottom'></div>
-      <div className='penalty-spot top'></div>
-      <div className='penalty-spot bottom'></div>
+    <div className='fd-background'>
+      <div className='field'>
+        <div className='center-line'></div>
+        <div className='center-circle'></div>
+        <div className='penalty-area top'></div>
+        <div className='penalty-area bottom'></div>
+        <div className='goal-area top'></div>
+        <div className='goal-area bottom'></div>
+        <div className='goal top'></div>
+        <div className='goal bottom'></div>
+        <div className='penalty-spot top'></div>
+        <div className='penalty-spot bottom'></div>
+      </div>
+      <FieldList></FieldList>
+    </div>
+  );
+};
+
+const FieldList = () => {
+  return (
+    <div className='fieldList-container'>
+      <div className='fieldList-list'>
+        <div className='fieldList-num'>
+          <text className='fieldList-num-text'>1</text>
+        </div>
+        <text className='fieldList-mainText'>선수이름</text>
+      </div>
+      <div className='fieldList-list'></div>
     </div>
   );
 };
