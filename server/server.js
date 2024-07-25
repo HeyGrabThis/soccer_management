b4e14fe62646b0ec23c925f16ce03f0f7a3733d4
HeyGrabThis <qkqajrrhtj@naver.com>
2024-07-25 22:47:31 +0900

    오른쪽 탭 완성

--- server/server.js
+++ server/server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { MongoClient } = require('mongodb');
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+let db;
+const url =
+  'mongodb+srv://qkqajrrhtj:dhzpdl6632@cluster0.4vycw4p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log('DB연결성공');
+    db = client.db('field');
+    app.listen(8080, function () {
+      console.log('listening on 8080');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.send('루트');
 });
 
-app.listen(8080, function () {
-  console.log('listening on 8080');
-});
-
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
